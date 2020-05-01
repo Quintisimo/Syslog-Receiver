7e13c66832336a91d0bf161ae5f2ea7760dfd55e
Quintus <quintuscardozo13@gmail.com>
2020-05-01 17:04:05 +1000

    Initial Commit

---
+++ Log.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+
+namespace SyslogReceiver
+{
+    class Log
+    {
+        private readonly string[] FACILITIES = { 
+            "kernal message",
+            "user-level message",
+            "Mail System",
+            "system daemons",
+            "security/authorization messages",
+            "messages generated internally by syslogd",
+            "line printer subsytem",
+            "network news subsystem",
+            "UUCP subsystem",
+            "clock daemon",
+            "security/authorization messages",
+            "FTP daemon",
+            "NTP subsystem",
+            "log audit",
+            "log alert",
+            "clock daemon (note 2)",
+            "local use 0  (local0)",
+            "local use 1  (local1)",
+            "local use 2  (local2)",
+            "local use 3  (local3)",
+            "local use 4  (local4)",
+            "local use 5  (local5)",
+            "local use 6  (local6)",
+            "local use 7  (local7)"
+        };
+
+
+        private readonly string[] SEVERITIES = {
+              "Emergency: system is unusable",
+              "Alert: action must be taken immediately",
+              "Critical: critical conditions",
+              "Error: error conditions",
+              "Warning: warning conditions",
+              "Notice: normal but significant condition",
+              "Informational: informational messages",
+              "Debug: debug-level messages",
+        };
+
+        private int facility;
+        private int severity;
+
+        public int Facility 
+        { 
+            set
+            {
+                if (value > FACILITIES.Length) throw new ArgumentException("Invalid facility code");
+                facility = value;
+            }
+        }
+
+        public string getFacility()
+        {
+            return FACILITIES[facility];
+        }
+
+        public int Severity 
+        {
+            set
+            {
+                if (value > SEVERITIES.Length) throw new ArgumentException("Invalid severity code");
+                severity = value;
+            }
+        }
+
+        public string getSeverity()
+        {
+            return SEVERITIES[severity];
+        }
+
+        public int Version { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Hostname { get; set; }
+
+        public string Msg { get; set; }
+
+        public string AppName { get; set; }
+
+        public int ProcId { get; set; }
+
+        public int MsgId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SyslogReceiver
+{
+    class Program
+    {
+        private const int PORT = 11000;
+        private const string DIRECTORY = @"D:\User\Documents\Logs";
+        private const string FILE_NAME = "log";
+
+        private const string TEST_MESSAGE = "<100>2 1982-07-10T20:30:40.001Z myserver.com su 201 32001 - BOM 'su root' failed on /dev/pts/7";
+        private const string PRIORITY_REGEX = @"(?<=\<)(.*?)(?=\>)";
+        private const string VERSION_REGEX = @"(?<=\>)\d+";
+
+        private static List<string> Queue = new List<string>();
+        private static readonly object writerLock = new object();
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread listenerThread = new Thread(StartServer);
+                listenerThread.Start();
+
+                Thread testerThread = new Thread(Tester);
+                testerThread.Start();
+
+                Task.Run(() => WriterTask());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        /// <summary>
+        /// Start syslog server
+        /// </summary>
+        private static void StartServer()
+        {
+            UdpClient listener = new UdpClient(PORT);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
+            Console.WriteLine("Waiting for message");
+
+            try
+            {
+                while(true)
+                {
+                    byte[] bytes = listener.Receive(ref endPoint);
+                    string data = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+
+                    lock(writerLock)
+                    {
+                        Queue.Add(data);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        /// <summary>
+        /// Convert queue message to Log class
+        /// </summary>
+        private static void WriterTask()
+        {
+            try
+            {
+                while(true)
+                {
+                    Task.Delay(1000).Wait();
+
+                    lock(writerLock)
+                    {
+                        foreach (string message in Queue)
+                        {
+                            string[] messageArr = message.Split();
+                            int priority = int.Parse(GetValue(PRIORITY_REGEX, messageArr[0]));
+
+                            int facility = priority / 8;
+                            int severity = priority - (facility * 8);
+                            int version = int.Parse(GetValue(VERSION_REGEX, messageArr[0]));
+                            DateTime timeStamp = DateTime.Parse(messageArr[1]);
+                            string hostname = messageArr[2];
+                            string appName = messageArr[3];
+                            int procId = int.Parse(messageArr[4]);
+                            int msgId = int.Parse(messageArr[5]);
+                            string msg = string.Join(" ", new ArraySegment<string>(messageArr, 8, messageArr.Length - 8));
+
+                            Log log = new Log
+                            {
+                                Hostname = hostname,
+                                Timestamp = timeStamp,
+                                Msg = msg,
+                                Facility = facility,
+                                Severity = severity,
+                                Version = version,
+                                AppName = appName,
+                                ProcId = procId,
+                                MsgId = msgId
+                            };
+
+                            WriteToFile(log);
+                            Console.WriteLine($"Received from {hostname}");
+                            Console.WriteLine($"Data: {message}{Environment.NewLine}");
+                        }
+                        Queue = new List<string>();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        /// <summary>
+        /// Get field from message using regex
+        /// </summary>
+        /// <param name="regexPattern">regex pattern</param>
+        /// <param name="message">message string</param>
+        /// <returns></returns>
+        private static string GetValue(string regexPattern, string message)
+        {
+            Regex regex = new Regex(regexPattern);
+            Match match = regex.Match(message);
+            return match.Value;
+        }
+
+        private static void Tester()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+
+            byte[] send = Encoding.ASCII.GetBytes(TEST_MESSAGE);
+            IPEndPoint endPoint = new IPEndPoint(broadcast, 11000);
+
+            while (true)
+            {
+                s.SendTo(send, endPoint);
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// <summary>
+        /// Write log to file
+        /// </summary>
+        /// <param name="log">log row</param>
+        private static void WriteToFile(Log log)
+        {
+            string fileName = Path.Combine(DIRECTORY, $"{FILE_NAME} {DateTime.Now:dd-MM-yyyy}.txt");
+            
+            if (!Directory.Exists(DIRECTORY))
+            {
+                Directory.CreateDirectory(DIRECTORY);
+            }
+
+            if (!File.Exists(fileName))
+            {
+                using (FileStream fsCreate = File.Create(fileName))
+                {
+                    Byte[] header = new UTF8Encoding(true).GetBytes($"Created at {DateTime.Now:dd-MM-yyyy}{Environment.NewLine}{Environment.NewLine}");
+                    fsCreate.Write(header, 0, header.Length);
+                }
+            }
+
+            using (StreamWriter fsAppend= File.AppendText(fileName))
+            {
+                fsAppend.WriteLine($"Host: {log.Hostname}");
+                fsAppend.WriteLine($"Version: {log.Version}");
+                fsAppend.WriteLine($"Timestamp: {log.Timestamp}");
+                fsAppend.WriteLine($"Severity: {log.getSeverity()}");
+                fsAppend.WriteLine($"Facility: {log.getFacility()}");
+                fsAppend.WriteLine($"Message: {log.Msg}{Environment.NewLine}");
+            }
+        }
+    }
+}
